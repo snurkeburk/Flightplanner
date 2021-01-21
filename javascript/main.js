3c030719cfc9db08f9ad3b76ad8d09b627c19fc9
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2021-01-21 19:33:43 +0100

    added serveral new airports, made "random route generator", added script to draw lines between selected airports

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,9 @@
 //import selected route from route.js and draw line between dots
+
+let airports = ['ESSA','ESSB','ENGM','KORD', 'EHAM','EGKK','KLAS','KLAX','LIRN','EFHK'];
+
 function test() {
 var depname = document.getElementById("dep").value;
-let airports = ['ESSA','ESSB','ENGM','KORD', 'EHAM','EGKK'];
-let airportsS = ['ESSA','ESSB','ENGM','KORD', 'EHAM','EGKK'];
 var brightMap = document.getElementById("map-bright");
 var darkMap = document.getElementById("map-dark");
 var i = 0;
@@ -26,6 +27,7 @@ while(i < airports.length){
             i = i+1;
         }
     }
+        rect();
         i = airports.length;
         document.getElementById(depname).style.backgroundColor = "lightgreen";
 
@@ -36,7 +38,6 @@ while(i < airports.length){
     }
     
 }
-
 }
 
 function colorChange(){
@@ -44,6 +45,49 @@ function colorChange(){
     var arrname = document.getElementById("arr").value;
 
 }
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+// example use
+function rect(){
+var depname = document.getElementById('dep').value;
+var arrname = document.getElementById("arr").value;
+var div = document.getElementById(depname);
+var divOffset = offset(div);
+var divsecond = document.getElementById(arrname);
+var divsecondOffset = offset(divsecond);
+console.log(divsecondOffset.left, divsecondOffset.top);
+console.log(divOffset.left, divOffset.top);
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'green';
+
+ctx.moveTo(divsecondOffset.left, divsecondOffset.top);
+ctx.lineTo(divOffset.left, divOffset.top);
+ctx.stroke();
+
+}
+
+
+function randomRoute() {
+    var randomDeparture = Math.floor(Math.random() * airports.length);
+    var departure = airports[randomDeparture];
+    document.getElementById('dep').value = departure;
+    document.getElementById(departure).style.backgroundColor = "lightgreen";   
+    var randomArrival = Math.floor(Math.random() * airports.length);
+    var arrival = airports[randomArrival];
+    document.getElementById('arr').value = arrival;
+    document.getElementById(arrival).style.backgroundColor = "lightgreen";   
+
+    rect();
+}
+
 
 //dark mode 
 //            darkMap.style.display = "none";
