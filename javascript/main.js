7127182d99e511810437029d0db12de22d109093
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2021-01-21 15:59:47 +0100

    made airport array for script to find the desired route, highlighted the selected airports with green color when entered in input boxes

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,50 @@
 //import selected route from route.js and draw line between dots
+function test() {
+var depname = document.getElementById("dep").value;
+let airports = ['ESSA','ESSB','ENGM','KORD', 'EHAM','EGKK'];
+let airportsS = ['ESSA','ESSB','ENGM','KORD', 'EHAM','EGKK'];
+var brightMap = document.getElementById("map-bright");
+var darkMap = document.getElementById("map-dark");
+var i = 0;
+while(i < airports.length){
+    var depname = document.getElementById("dep").value;
+    var arrname = document.getElementById("arr").value;
+
+    colorChange();
+
+    if (airports[i] == depname){
+        console.log(depname);
+        i = 0;
+        while(i < airports.length){
+        if(airports[i] == arrname){
+            console.log(arrname);
+            i = airports.length;
+            document.getElementById(arrname).style.backgroundColor = "lightgreen";
+
+        } else {
+            console.log("arrival not found, trying again...")
+            i = i+1;
+        }
+    }
+        i = airports.length;
+        document.getElementById(depname).style.backgroundColor = "lightgreen";
+
+        //get coords
+    } else {
+        console.log("departure not found, trying again...");
+        i = i+1;
+    }
+    
+}
+
+}
+
+function colorChange(){
+    var depname = document.getElementById("dep").value;
+    var arrname = document.getElementById("arr").value;
+
+}
+
+//dark mode 
+//            darkMap.style.display = "none";
+//            brightMap.style.display = "block";
